88751cbb2a11fdfad0eee2cbe3b08e7a2230645c
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2023-06-06 13:02:13 -0600

    password hashing and verifying

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import Optional, List
 from db import users
+from passlib.context import CryptContext
+
+pwd_ctx = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def hash_password(plain_password: str) -> str:
+    return pwd_ctx.hash(plain_password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_ctx.verify(plain_password, hashed_password)
 
 
 class Notification(BaseModel):
@@ -36,4 +47,4 @@ def root(request: Request):
 
 @app.get('/login', response_class=HTMLResponse)
 def get_login(request: Request):
-    return templates.TemplateResponse('login.html', {"request": request, "title": "FriendConnect - Login", "invalid": True})
+    return templates.TemplateResponse('login.html', {"request": request, "title": "FriendConnect - Login"})
